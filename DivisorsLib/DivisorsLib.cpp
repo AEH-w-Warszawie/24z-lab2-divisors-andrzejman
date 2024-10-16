50a28bb543cdc48186228c0a0a3945a8a743ed44
andrzejman <a.manujlo@vizja.pl>
2024-10-16 13:20:34 +0200

    Improved algorythm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -6,6 +6,7 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 	std::vector<unsigned long long> result;
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
+		//There is upedated code (check the lecture how to do this)
 		if (number % i == 0)
 			result.push_back(i);
 	}
