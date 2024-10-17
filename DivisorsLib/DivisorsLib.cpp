bec7ed11f32541de45fae77c3f951237ed0905be
andrzejman <a.manujlo@vizja.pl>
2024-10-17 15:51:36 +0200

    Faster Algorythm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -6,8 +6,7 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 	std::vector<unsigned long long> result;
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
-		// write more fast algorithm
-		//write here fatser algorithm (was discussed on lecture)
+		// faster alg
 		if (number % i == 0)
 			result.push_back(i);
 	}
